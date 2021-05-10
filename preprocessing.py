c5a6f86fd4fc316a2165558a14cb89e9272f7a10
Dev Parikh <47342287+devparikh@users.noreply.github.com>
2021-05-10 07:52:37 -0400

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import numpy as np
 import matplotlib.pyplot as plt
 
 # we need to read in the data
@@ -24,19 +25,19 @@ def reading_in_image(path, dataset):
 
         # we are rotating the initial images by 90 degrees counterclockwise and add that to do the dataset
         rotated_image = cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
-        dataset(rotated_image)
+        dataset.append(rotated_image)
         
         # we are rotating the initial images by 90 degrees clockwise and adding it to the dataset
         rotated_image_2 = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
-        dataset(rotated_image_2)
+        dataset.append(rotated_image_2)
 
         # doing a horizontal flip on the original image and then adding it to the dataset
         horizontally_flipped_image = cv2.flip(image, -1)
-        dataset(horizontally_flipped_image)
+        dataset.append(horizontally_flipped_image)
 
         # doing a vertical flip on the original image and then adding it to the dataset
         vertically_flipped_image = cv2.flip(image, 0)
-        dataset(vertically_flipped_image)
+        dataset.append(vertically_flipped_image)
 
         # now we are creating a maxtrics that will contain all of the nessesary values to create a image translation
         # the matrix that we are creating have have these values
@@ -48,7 +49,7 @@ def reading_in_image(path, dataset):
         # negative values for ty shifts the image up
         # positive values for ty will shift the image down
         # this matrix can do operations with 32 bits and is much faster than float64
-        Matrix = np.float32([1,0,15],
+        Matrix = np.float32([[1,0,15],
                             [0,1,-10]
                             ])
         # cv2.warpAffine does the translation for us 
@@ -56,6 +57,50 @@ def reading_in_image(path, dataset):
         # the third parameter is the width and height of the original image
         shifted_image = cv2.warpAffine(image, Matrix, (image.shape[0], image.shape[1]))
         dataset.append(shifted_image)
+
+        # now if we want to do a reflection we will have to use a different type of Matrix that can be used to do the reflection
+        # here is the matrix for the x-axis reflection:
+        # M = [1, 0, 0,
+        #      0, -1, rows,
+        #      0,  0,  1]
+        # in the case of an x-axis reflection we can set sy = -1 and sx = 1 and vice versa for the y-axis
+        # here is the matrix for the y-axis reflection:
+        # M = [-1, 0, 0,
+        #      0, 1, rows,
+        #      0,  0,  1]
+        
+        rows = 256
+        cols = 256
+        
+        # we are only doing a x-axis reflection here
+        M = np.float32([[1, 0, 0], 
+                        [0, -1, rows],
+                        [0,  0, 1]])
+       
+        # after defining the matrix we can use cv2.warpPerpective which will warp(bent or twist) the perspective of the image
+        # the first parameter is the image and the second parameter is the matrix
+        # the third parameter is the integer values of the variables rows and cols
+        reflected_image = cv2.warpPerspective(image, M, (cols, rows))
+        dataset.append(reflected_image)
+
+        # the final augmentation that we are going to do is to do all of the augmentations we did before but all on one image and then add that image to the dataset
+        augmented_image = cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
+        augmented_image = cv2.rotate(image, cv2.ROTATE_180)
+
+        augmented_image = cv2.flip(image, -1)
+        augmented_image = cv2.flip(image, 0)
+
+        Matrix = np.float32([[1,0,15],
+                            [0,1,-10]
+                            ])
+        augmented_image = cv2.warpAffine(image, Matrix, (image.shape[0], image.shape[1]))
+
+        M = np.float32([[1, 0, 0], 
+                        [0, -1, rows],
+                        [0,  0, 1]])
+        augmented_image =  cv2.warpPerspective(image, M, (cols, rows))
+
+        dataset.append(augmented_image)
 
 # applying the function on the training and testing sets that we have
 reading_in_image(train_path, training_data)
@@ -65,5 +110,3 @@ reading_in_image(val_path, validation_data)
 print(len(training_data))
 # length of the testing data
 print(len(validation_data))
-        
-
